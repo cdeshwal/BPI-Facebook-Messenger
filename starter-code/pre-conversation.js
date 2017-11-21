adac49bf3fb8d7f1c63a61c5006130d5d0af6b31
cdeshwal <cdeshwal@gmail.com>
2017-11-21 22:32:57 +0800

    Update pre-conversation.js

--- starter-code/pre-conversation.js
+++ starter-code/pre-conversation.js
@@ -1,3 +1,6 @@
+
+
+
 /**
  * Copyright IBM Corp. 2017
  *
@@ -15,15 +18,104 @@
  */
 
 'use strict';
+const request= require('request');
 
 /**
  * Here the user can edit the channel input message before send to Conversation module.
  *
  * @param  {JSON} params - input JSON sent by the channel module
  * @return {JSON}        - modified input JSON to be sent to the conversation module
- */
+// */
 function main(params) {
-  return Promise.resolve(params);
+  return new Promise((resolve, reject) => {
+  
+
+    if(params.raw_input_data.provider=='facebook' && params.raw_input_data.auth)
+    {
+      const auth = params.raw_input_data.auth;
+
+      const getUrl = 'https://graph.facebook.com/v2.6/'+params.raw_input_data.facebook.sender.id
+
+      return getFacebookUsername(
+        params,
+        getUrl,
+        auth.facebook.page_access_token
+        
+      ).then(resolve).catch(reject)
+       }
+    else
+    {
+      
+      return Promise.resolve(params);
+      
+      
+      
+    }
+  
+  
+  
+  
+
+
+  
+
+ });                     
+  
+}
+
+
+function getFacebookUsername(params, getUrl, accessToken) {
+  return new Promise((resolve, reject) => {
+    
+        request(
+      {
+        url: getUrl,
+        qs: { access_token: accessToken },
+        method: 'GET',
+      },
+      (error, response) => {
+        if (error) {
+          var params2= JSON.parse(JSON.stringify(params));
+          params2.conversation.context.Fbusername=''
+          reject(params2);
+        }
+        if (response) {
+          if (response.statusCode === 200) {
+            // Facebook expects a "200" string/text response instead of a JSON.
+            // With Cloud Functions if we have to return a string/text, then we'd have to specify
+            // the field "text" and assign it a value that we'd like to return. In this case,
+            // the value to be returned is a statusCode.
+            var params2= JSON.parse(JSON.stringify(params));
+            var Fbusername = JSON.parse(response.body).first_name
+            params2.conversation.context.Fbusername=Fbusername
+            resolve(params2);
+          }
+          else
+          {
+            var params2= JSON.parse(JSON.stringify(params));
+            params2.conversation.context.Fbusername=''
+            reject(params2);
+          }
+          
+        }
+        else
+        {
+          var params2= JSON.parse(JSON.stringify(params));
+          params2.conversation.context.Fbusername=''
+          reject(params);
+        }
+        
+        
+      }
+    );
+   
+
+
+
+   
+
+
+  });
 }
 
 module.exports = main;
